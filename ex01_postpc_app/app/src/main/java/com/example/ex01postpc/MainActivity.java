17c2299a06d1060232340bdbb260ab1a2ca3031e
Alon Emanuel <alon@alonemanuel.com>
2020-04-04 10:31:19 +0300

    Text working as wanted.

--- ex01_postpc_app/app/src/main/java/com/example/ex01postpc/MainActivity.java
+++ ex01_postpc_app/app/src/main/java/com/example/ex01postpc/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ex01postpc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button createButton = findViewById(R.id.createButton);
+        final EditText userInput = findViewById(R.id.userInput);
+        final TextView textLabel = findViewById(R.id.usersTextView);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userInput.setText(' ');
+                textLabel.setText(userInput.getText());
+            }
+        });
+
     }
+
 }

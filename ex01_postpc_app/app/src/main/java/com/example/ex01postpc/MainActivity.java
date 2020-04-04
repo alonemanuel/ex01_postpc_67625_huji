0efefb5ec7fc7455a03083e0bd526d28f6455626
Alon Emanuel <alon@alonemanuel.com>
2020-04-04 11:04:10 +0300

    Hiding keyboard now works

--- ex01_postpc_app/app/src/main/java/com/example/ex01postpc/MainActivity.java
+++ ex01_postpc_app/app/src/main/java/com/example/ex01postpc/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.ex01postpc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,23 @@ public class MainActivity extends AppCompatActivity {
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userInput.setText(' ');
+                hideKeyboard();
                 textLabel.setText(userInput.getText());
+                userInput.setText("");
             }
         });
 
     }
 
+    // keyboard hider
+    public void hideKeyboard() {
+        try {
+            InputMethodManager inputmanager = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (inputmanager != null) {
+                inputmanager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+            }
+        } catch (Exception var2) {
+        }
+
+    }
 }
